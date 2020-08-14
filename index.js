95c96c190caed0df56b0473c4711ae381cf59def
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-14 13:55:19 +0530

    adding mongoose

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express  = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
-
+mongoose.connect(keys.mongoURI);
 const PORT = process.env.PORT  || 5000;
 app.listen(PORT);
