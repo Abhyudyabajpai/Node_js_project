e94f71a1e8b5a1a62ac5b9b6b3d32ba0edafeb97
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-17 13:31:18 +0530

    connect mongoose to mongoDB

--- services/passport.js
+++ services/passport.js
@@ -14,7 +14,16 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile,done) =>{
-            new User({googleId: profile.id}).save();
+            User.findOne({googleId: profile.id})
+                .then((existingUser) => {
+                    if(existingUser){
+
+                    }
+                    else{
+                        new User({googleId: profile.id}).save();
+                    }
+                })
+            
         }
     )
     );
