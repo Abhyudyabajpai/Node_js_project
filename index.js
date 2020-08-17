a81cef01bb4ab9cbdb1d3bb246f27af563d7819d
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-17 14:03:45 +0530

    adding cookie session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express  = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -8,6 +10,14 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(
+    cokkieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey] 
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
