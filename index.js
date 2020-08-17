de2f0dcb63f6c4c9bf1342905fb208405e518870
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-17 14:48:38 +0530

    adding cookie session

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 app.use(
-    cokkieSession({
+    cookieSession({
         maxAge: 30*24*60*60*1000,
         keys: [keys.cookieKey] 
     })
