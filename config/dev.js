12964b81729609cad444bcf89726aa33286fed25
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-19 17:41:10 +0530

    emaily_prod

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js  do not commit this!
+
+module.exports = {
+    googleClientID: '363633425311-4ah580njbgs031u2oi2h869ua3ghvorq.apps.googleusercontent.com',
+    googleClientSecret: '_-RHbgLfie5qb50RnV6BxxXG',
+    mongoURI: 'mongodb+srv://abhyudya:s3noNlXdRvLaCknW@emaily.unw4b.mongodb.net/emaily?retryWrites=true&w=majority',
+    cookieKey: "dsifscsicsdfoadcosacnodncodancodanco"
+};
