f87251a21a3cf5f1c7d9eec4d2bd9e705646426c
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-24 00:24:39 +0530

    adding redux-thunk

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 
@@ -8,21 +10,26 @@ const SurveyNew =  () => <h2>SurveyNew</h2>
 const Landing =  () => <h2>Landing</h2>
 
 
-const App =() => {
-    return (
-        <div className = "container">
-            <BrowserRouter>
-            <div>
-                <Header />
-                <Route exact path ='/' component = {Landing} />
-                <Route exact path = '/surveys' component = {Dashboard} />
-                <Route path = '/surveys/new' component = {SurveyNew}/>
+class App extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+    render (){
+        return (
+            <div className = "container">
+                <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path ='/' component = {Landing} />
+                    <Route exact path = '/surveys' component = {Dashboard} />
+                    <Route path = '/surveys/new' component = {SurveyNew}/>
 
+                </div>
+                </BrowserRouter>
             </div>
-            </BrowserRouter>
-        </div>
-    
-    );
-};
+        
+        );
 
-export default App;
+    }
+}
+export default connect(null,actions) (App);
