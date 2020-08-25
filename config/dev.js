2aa5ff01e37a038f81cf03fe3f6d218dafe6b976
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-25 23:59:25 +0530

    connecting Landing page

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     googleClientID: '363633425311-4ah580njbgs031u2oi2h869ua3ghvorq.apps.googleusercontent.com',
     googleClientSecret: '_-RHbgLfie5qb50RnV6BxxXG',
     mongoURI: 'mongodb+srv://abhyudya:s3noNlXdRvLaCknW@emaily.unw4b.mongodb.net/emaily?retryWrites=true&w=majority',
-    cookieKey: "dsifscsicsdfoadcosacnodncodancodanco"
+    cookieKey: "dsifscsicsdfoadcosacnodncodancodanco",
+    stripePublishableKey: 'pk_test_51HK6YmJj9MRaJgg6VoA0hBfrYRZJMUpnmMYoAmobxYaJdfhrLuOMf8ZKsSXNC03iAXgOzCDoS9EnlkM5KYVMlcwF00FQgoYUzX',
+    stripeSecretKey: 'sk_test_51HK6YmJj9MRaJgg6Nx4ZJJYOLO4jRGTxmY4lvx71HMDfZ4IMs5epzqCbrnVlAvGxOvXmBNkWkSMcIRLA2Raap57q00Idt19fA2'
 };
