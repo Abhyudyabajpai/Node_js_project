5ce5c3de253ab69de04509915f46a7fa9549b2c1
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-28 15:44:15 +0530

    billing_routes

---
+++ routes/billingRoutes.js
@@ -0,0 +1,7 @@
+const keys = require('../config/keys');
+const stripe =require('stripe')(keys.stripeSecretKey);
+
+module.exports = app => {
+    app.post('/api/stripe', (req, res)=> {
+        });
+};
