36f06cc0e82dbb82c3df5ec08eff85b8bd210fb7
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-08-28 17:09:11 +0530

    billing_routes

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,27 @@
 const keys = require('../config/keys');
 const stripe =require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middleware/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res)=> {
+    app.post('/api/stripe',requireLogin, async (req, res)=> {
         const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: '$5 for 5 credits',
-            source : req.body.id
+            shipping: {
+                name: 'Abhyudya',
+                address:{
+                line1: '510 Townsend St',
+                postal_code: '98140',
+                city: 'CA',
+                country: 'US',
+                },
+            },
+                amount: 500,
+                currency: 'usd',
+                description: '$5 for 5 credits',
+                source : req.body.id
         });
-        console.log(charge);
+        req.user.credits +=5;
+        const user = await req.user.save();
+        
+        res.send(user);
     });
 };
