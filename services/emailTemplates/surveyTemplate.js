1705958359cdf82c5958961ab38cade31ca2aed9
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-09-05 21:33:31 +0530

    sendgrid

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -3,15 +3,15 @@ module.exports = (survey)=> {
     <html>
         <body>
             <div style ="text-align: center;">
-            <h3>I'd like your inout!</h3>
+            <h3>I'd like your input!</h3>
             <p>Please answer the following question:</p>
             <p>${survey.body}</p>
-            <div>
-            <a href = "http://localhost:3000'>Yes</a>
-            </div>
-            <div>
-            <a href = "http://localhost:3000'>No</a>
-            </div>
+                <div>
+                    <a href = "http://localhost:3000'>Yes</a>
+                </div>
+                <div>
+                    <a href = "http://localhost:3000'>No</a>
+                </div>
             </div>
         </body>
     </html>
