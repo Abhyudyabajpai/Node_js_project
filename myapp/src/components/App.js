996684f06626d1cf47ad69c3c0245566a6fd2739
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-09-15 13:09:09 +0530

    SurveyForm

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -5,8 +5,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-
-const SurveyNew =  () => <h2>SurveyNew</h2>
+import SurveyNew from './surveys/SurveyNew';
 
 
 
---
+++ myapp/src/components/surveys/SurveyForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+class SurveyForm extends Component {
+    render() {
+        return (
+            <div>
+                <form onSubmit = {this.props.handleSubmit(values => console.log(values))}
+                >
+                    < Field type ='text' name= 'surveyTitle' component ='input'/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'surveyForm'
+}) (SurveyForm);
