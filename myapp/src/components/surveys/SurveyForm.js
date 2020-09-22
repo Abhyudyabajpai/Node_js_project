bd28732cc0ee79f36931059bf15d8691e1d87d90
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-09-22 22:56:25 +0530

    Validation

--- myapp/src/components/surveys/SurveyForm.js
+++ myapp/src/components/surveys/SurveyForm.js
@@ -45,9 +45,13 @@ class SurveyForm extends Component {
 function validate(values){
     const errors ={};
 
-    if (!values.title){
-        errors.title = 'You must provide a title';
-    }
+    _.each(FIELDS,({name}) => {
+        if (!values[name]){
+            errors[name] = 'You must provide a value';
+        }
+    });
+
+  
 
     return errors;
 }
