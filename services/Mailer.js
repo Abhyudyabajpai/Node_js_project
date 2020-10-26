d1530db5408a17e9bda2a6b8acfea225a8aac6ad
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-10-26 17:57:21 +0530

    Sendgrid_api update

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,7 +8,7 @@ class Mailer extends helper.Mail {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('abhyudyabajpai10@gmail.com');
+        this.from_email = new helper.Email('vellscuran21@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html',content);
         this.recipients = this.formatAddresses(recipients);
