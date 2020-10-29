a744787a283e31ab4f714c48e1f21530b468fb5c
Abhyudya Bajpai <abhyudyabajpai10@gmail.com>
2020-10-29 13:21:35 +0530

    testing webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,6 +9,11 @@ module.exports = app => {
     app.get('/api/surveys/thanks',(req,res) => {
         res.send('Thanks for Voting!');
     });
+
+    app.post('/api/surveys/webhooks',(req,res) => {
+        console.log(req.body);
+        res.send({});
+    });
     app.post('/api/surveys',requireLogin,requireCredits, async (req,res) =>{
         const {title,subject,body,recipients} = req.body;
         
